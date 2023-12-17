a43c5371c5e170db55cf6afcbab43a3875747e40
Just_ne_Just <semakov_2002@mail.ru>
2023-12-17 14:39:44 +0300

    eval code

--- src/train/train.py
+++ src/train/train.py
@@ -79,6 +79,8 @@ def eval(model, dataloader, device, fixed_noise, fid_metric, ssim_metric):
     constructed_imgs = normalize(constructed_imgs, 0, 1)
     constructed_imgs = torch.cat(constructed_imgs)
 
+    print(real_imgs.shape, constructed_imgs.shape)
+
     fid = fid_metric.compute_metric(real_imgs.flatten(1), constructed_imgs.flatten(1)).cpu().numpy(),
     ssim = ssim_metric(real_imgs, constructed_imgs).item()
     return fid, ssim
